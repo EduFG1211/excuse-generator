696b0434e9a75015a3544eba3e288f6c7991c650
Edventures <edufg13@gmail.com>
2021-02-26 17:01:07 +0000

    proyecto terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,74 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  console.log("Hello Eduuuu from the console! ");
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let pronoun = ["My", "A", "The", "That", "This", "A ginormous", "A F*cking"];
+  let subject = [
+    "racoon",
+    "grandma",
+    "babysitter",
+    "crazy girlfriend",
+    "lizzard",
+    "hippo",
+    "puppet"
+  ];
+  let action = [
+    "kicked my",
+    "bit my",
+    "ate my",
+    "stole my",
+    "yelled at my",
+    "threw my",
+    "took my"
+  ];
+  let possesion = [
+    "car",
+    "homework",
+    "toe",
+    "nose",
+    "ass",
+    "little sister",
+    "ex-girlfriend",
+    "hobo"
+  ];
+  let where = [
+    "on the street",
+    "in front of the president",
+    "in the Wild Wild West",
+    "in a galaxy far far away",
+    "in Jumanji",
+    "in front of my house",
+    "in Neverland",
+    "in the driveway"
+  ];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possesionIndex = Math.floor(Math.random() * possesion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  let pronounSelected = pronoun[pronounIndex];
+  let subjectSelected = subject[subjectIndex];
+  let actionSelected = action[actionIndex];
+  let possesionSelected = possesion[possesionIndex];
+  let whereSelected = where[whereIndex];
+
+  return (
+    pronounSelected +
+    " " +
+    subjectSelected +
+    " " +
+    actionSelected +
+    " " +
+    possesionSelected +
+    " " +
+    whereSelected +
+    "."
+  );
 };
